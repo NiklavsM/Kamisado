562f9bc17a81b1d44039d6e2abcf1746366ee81d
Niklavs Meiers <niklavs@burti.lv>
2017-03-24 14:03:46 +0000

    Added Stats

---
+++ KamisadoWorkspace/KamisadoCS207/src/model/Stat.java
@@ -0,0 +1,40 @@
+package model;
+
+import java.io.Serializable;
+
+public class Stat implements Serializable {
+	private String name;
+	private int wins;
+	private int loses;
+
+	public Stat(String name) {
+		this.name = name;
+		wins = 0;
+		loses = 0;
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getWins() {
+		return wins;
+	}
+
+	public void addWins() {
+		wins++;
+	}
+
+	public int getLoses() {
+		return loses;
+	}
+
+	public void addLoses() {
+		loses++;
+	}
+}
