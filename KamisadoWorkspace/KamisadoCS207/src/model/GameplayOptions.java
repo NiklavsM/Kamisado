984a61d01c0a693c033bacaa0659613a187f6d31
Niklavs Meiers <niklavs@burti.lv>
2017-04-09 11:31:44 +0300

    Gameplay options > unfinsihed but had to commit.

---
+++ KamisadoWorkspace/KamisadoCS207/src/model/GameplayOptions.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.awt.Color;
+import java.io.Serializable;
+
+public class GameplayOptions implements Serializable{
+	Color[] boardColors;
+	public GameplayOptions(){
+		
+	}
+	
+	
+}
